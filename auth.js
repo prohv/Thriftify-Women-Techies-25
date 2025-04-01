cf7b9750aebb31024f3db756c9350953d8b03e01
Harsh Vardhan Singh <socialhv1@gmail.com>
2025-04-01 22:07:55 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
-};
+};/
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
